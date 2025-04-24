92bc0c1ea3632fe2798cc3df155676b890757772
DesLigaFacens <69490107+MRJackFrost@users.noreply.github.com>
2025-04-23 22:08:56 -0300

    Feat(Colliders)

---
+++ ProjektMonsterSoul/Assets/Game/Shared/Enemies/Slime/Scripts/SpikeController.cs
@@ -0,0 +1,14 @@
+using Game.Shared.Enemy;
+
+namespace Game.Shared.Enemies.Slime.Scripts
+{
+    public class SpikeController : EnemyController
+    {
+        protected override void OnDeath()
+        {
+        }
+        protected override void OnTakeDamage(int damage)
+        {
+        }
+    }
+}
